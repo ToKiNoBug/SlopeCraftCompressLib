f3a74e2a7dc6ad378f28a8569c265b0d7cf7d8f1
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-07-06 18:05:25 +0800

    重写并精简了大部分内容

--- optitree.cpp
+++ optitree.cpp
@@ -1,5 +1,164 @@
 #include "optitree.h"
 
+Node::Node(short beg,short end)
+{
+    Begin=beg;
+    End=end;
+    Degree=0;
+    Child=NULL;
+    Sib=NULL;
+    isAble=true;
+}
+
+void Node::disp()
+{
+    cout<<'['<<Begin<<','<<End<<']';
+    if(haveChild())
+    {
+        cout<<'{';
+        Child->disp();
+        cout<<'}';
+    }
+    if(haveSib())
+    {
+        cout<<'-';
+        Sib->disp();
+    }
+}
+
+inline bool Node::haveChild()
+{
+    return (Child!=NULL)&&(Child->isAble);
+}
+
+inline bool Node::haveSib()
+{
+    return (Sib!=NULL)&&(Sib->isAble);
+}
+
+inline bool Node::isComplete()
+{
+    return (Begin>=0)&&(End>=Begin);
+}
+
+Node* Node::creatChild()
+{
+    if(haveChild())
+    {
+        qDebug("已存在激活的子树，不能creatChild");
+        return Child;
+    }
+    if(Child==NULL)Child=new Node;
+    else
+        Child->isAble=true;
+    Child->Degree=Degree+1;
+    return Child;
+}
+
+Node* Node::creatSib()
+{
+    if(haveSib())
+    {
+        qDebug("已存在激活的侧链，不能creatSib");
+        return Sib;
+    }
+    if(Sib==NULL)Sib=new Node;
+    else
+        Sib->isAble=true;
+    Sib->Degree=Degree;
+    return Sib;
+}
+
+void Node::Freeze()
+{
+    if(!isAble)return;
+    isAble=false;
+    if(haveChild())Child->Freeze();
+    if(haveSib())Sib->Freeze();
+}
+
+
+OptiTree::OptiTree()
+{
+    Root=new Node;
+    Root->isAble=true;
+    Root->Degree=0;
+    Stack.push(Root);
+}
+
+inline Node* OptiTree::Current()
+{
+    return Stack.top();
+}
+
+inline short Node::Length()
+{
+    return End-Begin+1;
+}
+
+Node::~Node()
+{
+    if(Child!=NULL)delete Child;
+    if(Sib!=NULL)delete Sib;
+}
+
+void OptiTree::goDown()
+{
+    if(!Current()->haveChild())
+    {
+        qDebug("goDown失败");
+        return;
+    }
+    Stack.push(Current()->Child);
+    qDebug("goDown成功");
+}
+
+void OptiTree::goNextSib()
+{
+    if(!Current()->haveSib())
+    {
+        qDebug("goNextSib失败");
+        return;
+    }
+    Stack.push(Current()->Sib);
+    qDebug("goNextSib成功");
+}
+
+void OptiTree::goPrevSib()
+{
+    Node*Temp=Current();
+    Stack.pop();
+    if(Current()->Degree!=Temp->Degree)
+    {
+        qDebug("goPrevSib失败");
+        Stack.push(Temp);
+        return;
+    }
+    qDebug("goPrevSib成功");
+}
+
+void OptiTree::goUp()
+{
+    Node*Temp=Current();
+    while(!(Current()->Degree+1==Temp->Degree))Stack.pop();
+}
+
+void OptiTree::gotoRoot()
+{
+    preventEmpty();
+    while(Current()!=Root)Stack.pop();
+}
+
+inline void OptiTree::preventEmpty()
+{
+    if(Stack.empty())Stack.push(Root);
+}
+
+inline bool OptiTree::isRoot()
+{
+    return Current()==Root;
+}
+
 HeightLine::HeightLine(int _size,char method)
 {
     if (_size>0)Size=_size;
@@ -82,412 +241,11 @@ void HeightLine::toBrackets(vector<short> &index, string &brackets)
 
 }
 
-inline void HeightLine::Sink(idpRegion & rg)
+inline void HeightLine::Sink(Node*rg)
 {
-    if(rg.isEmpty()||rg.getEnd()>=Size)return;
-    qDebug()<<"尝试沉降["<<rg.getBegin()<<','<<rg.getEnd()<<']';
-    Height.segment(rg.getBegin(),rg.Length()).array()-=Height.segment(rg.getBegin(),rg.Length()).minCoeff();
-    qDebug("沉降了一次");
-}
-idpRegion::idpRegion(short beg,short end)
-{
-    Begin=beg;End=end;
-}
-
-inline idpRegion::idpRegion(const idpRegion& parent)
-{
-    Begin=parent.Begin;End=parent.End;
-}
-
-inline idpRegion idpRegion::operator=(const idpRegion& parent)
-{
-    return idpRegion(parent);
-}
-
-inline bool idpRegion::isEmpty()
-{
-    return !(Begin>=0&&End>=Begin);
-}
-
-inline void idpRegion::setBegin(short beg)
-{
-    /*if(!isEmpty()||Begin<0)
-    {qDebug("本idpRegion已有效/输入的Begin值无效，本次赋值无效");
-        return;}*/
-    Begin=beg;
-}
-
-inline void idpRegion::setEnd(short end)
-{
-    /*if(!isEmpty()||End<Begin)
-    {qDebug("本idpRegion已有效/输入的End值无效，本次赋值无效");
-        return;}*/
-    End=end;
-}
-
-inline void idpRegion::setEmpty()
-{
-    Begin=-1;End=-1;
-}
-
-inline short idpRegion::getBegin()
-{
-    return Begin;
-}
-
-inline short idpRegion::getEnd()
-{
-    return End;
-}
-
-inline bool idpRegion::operator==(const idpRegion & b)
-{
-    return (Begin==b.Begin)&&(End==b.End);
-}
-
-inline bool idpRegion::operator<(const idpRegion & b)
-{
-    return (Begin>b.Begin)&&(End<b.End);
-}
-
-inline bool idpRegion::operator<=(const idpRegion & b)
-{
-    return (Begin>=b.Begin)&&(End<=b.End);
-}
-
-inline bool idpRegion::operator>(const idpRegion & b)
-{
-    return (Begin<b.Begin)&&(End>b.End);
-}
-
-inline bool idpRegion::operator>=(const idpRegion & b)
-{
-    return (Begin<=b.Begin)&&(End>=b.End);
-}
-
-inline bool idpRegion::operator!=(const idpRegion & b)
-{
-    return (Begin=!b.Begin)||(End!=b.End);
-}
-
-inline short idpRegion::Length()
-{
-    return End-Begin+1;
-}
-
-Node::Node(short beg,short end)
-{
-    Value.setBegin(beg);Value.setEnd(end);
-    Child=NULL;Sib=NULL;isAble=true;
-    Degree=-1;
-}
-
-Node::Node(const idpRegion& p)
-{
-    Value=p;Child=NULL;Sib=NULL;
-    isAble=true;
-    Degree=-1;
-}
-
-void Node::setEnable(bool able)
-{
-    isAble=able;
-    Value.setEmpty();
-    if(!able)
+    if(rg->isComplete())
     {
-        if(haveChild())Child->setEnable(able);
-        if(haveSib())Sib->setEnable(able);
+        Height.segment(rg->Begin,rg->Length()).array()-=Height.segment(rg->Begin,rg->Length()).minCoeff();
+        qDebug()<<"沉降了["<<rg->Begin<<','<<rg->End<<']';
     }
-}
-
-inline void Node::creatChild()
-{
-    if(Child==NULL)
-    {
-        Child=new Node;
-        qDebug("new了新Child节点");
-    }
-
-    Child->setEnable(true);
-    Child->Value.setEmpty();
-    Child->Degree=Degree+1;
-}
-
-inline void Node::creatSib()
-{
-    if(Sib==NULL)
-    {
-        Sib=new Node;
-        qDebug("new了新的Sib节点");
-    }
-    Sib->setEnable(true);
-    Sib->Value.setEmpty();
-    Sib->Degree=Degree;
-}
-
-inline bool Node::isLeaf()
-{
-    return (Child==NULL||!Child->isAble);
-}
-
-Node::~Node()
-{
-    if(Child!=NULL)delete Child;
-    if(Sib!=NULL)delete Sib;
-}
-
-inline bool Node::haveChild()
-{
-    return (Child!=NULL&&Child->isAble);
-}
-
-inline bool Node::haveSib()
-{
-    return (Sib!=NULL&&Sib->isAble);
-}
-
-inline void Node::setValue(short beg,short end)
-{
-    Value=idpRegion(beg,end);
-}
-
-void Node::disp()
-{
-    //深度优先遍历，Self,Child,Sib
-    //[0,15]{[1,14]{}--[2,13]}
-    cout<<'['<<Value.getBegin()<<','<<Value.getEnd()<<']';
-    if(haveChild())
-    {
-        cout<<'{';
-        Child->disp();
-        cout<<'}';
-    }
-    if(haveSib())
-    {
-        cout<<'-';
-              Sib->disp();
-    }
-}
-
-OptiTree::OptiTree()
-{
-Root=new Node;
-Root->Degree=0;
-Root->Sib=NULL;
-Root->Child=NULL;
-Root->setEnable(true);
-Root->Value.setEmpty();
-Stack.push(Root);
-preventEmpty();
-}
-
-OptiTree::~OptiTree()
-{
-    delete  Root;
-}
-
-inline Node* OptiTree::Current()
-{
-    return Stack.top();
-}
-
-void OptiTree::goNextSib()
-{
-    preventEmpty();
-    if(Stack.top()->haveSib())
-    {Stack.push(Stack.top()->Sib);
-    qDebug("goNextSib成功");}
-    else qDebug("goNextSib失败：Sib不存在或失活");
-}
-
-void OptiTree::goDown()
-{
-    preventEmpty();
-    if(Stack.top()->haveChild())
-    {Stack.push(Stack.top()->Child);
-    qDebug("goDown成功");}
-    else qDebug("goDown失败：Child不存在或失活");
-}
-
-inline bool OptiTree::isRoot()
-{
-    preventEmpty();
-    return Stack.top()==Root;
-}
-
-void OptiTree::goPrevSib()
-{
-    preventEmpty();
-    if(isRoot()||Stack.empty()){qDebug("错误：goPrevSib已达到根节点或栈清空");return;}
-    Node*CurTemp=Stack.top();
-    Stack.pop();
-    if(Stack.empty()||Stack.top()->Degree!=CurTemp->Degree)
-        Stack.push(CurTemp);
-    else
-    {qDebug("goPrevSib成功");return;}
-    qDebug("goPrevSib失败：栈中前一个节点不是同深度的树");
-}
-
-void OptiTree::goUp()
-{
-    preventEmpty();
-    if(isRoot()||Stack.empty()){qDebug("错误：goUp已达到根节点或栈清空");return;}
-    Node *CurTemp=Stack.top();
-    Stack.pop();
-    if(Stack.top()->Degree+1==CurTemp->Degree&&Stack.top()->Child==CurTemp)
-    {
-        qDebug("goUp成功");
-        return;
-    }
-    else  goUp();
-}
-
-inline bool OptiTree::isLeaf()
-{
-    preventEmpty();
-    return Stack.top()->isLeaf();
-}
-
-inline void OptiTree::preventEmpty()
-{
-    if(Stack.empty())Stack.push(Root);
-    Root->Degree=0;
-}
-
-inline void OptiTree::gotoRoot()
-{
-    preventEmpty();
-    while(!isRoot())Stack.pop();
-}
-
-void OptiTree::addChild(short beg, short end)
-{
-    preventEmpty();
-    /*if(beg<Stack.top()->Value.getBegin())
-    {beg=Stack.top()->Value.getBegin();end=-1;}*/
-    if(Stack.top()->haveChild())
-    {
-        qDebug("此节点已经有子树");
-    }
-    else
-    Stack.top()->creatChild();
-
-    Stack.top()->Child->setValue(beg,end);
-}
-
-void OptiTree::addSib(short beg, short end)
-{
-    preventEmpty();/*
-    if(beg<Stack.top()->Value.getEnd())//Sib的起始不应小于左侧的末尾
-    {
-        Stack.top()->Value.getEnd();
-        end=-1;
-    }*/
-    if(Stack.top()->haveSib())
-    {
-        qDebug("此节点已经有侧链");
-    }
-    else
-    Stack.top()->creatSib();
-
-    Stack.top()->Sib->setValue(beg,end);
-}
-
-void OptiTree::Freeze()
-{
-    gotoRoot();
-    if(Root->haveChild())Root->Child->setEnable(false);
-    preventEmpty();
-    qDebug("Freeze完成");
-}
-
-void OptiTree::Clear()
-{
-    if(Root->Child!=NULL)delete Root->Child;
-    if(Root->Sib!=NULL)delete Root->Sib;//其实Root就不应该与Sib
-    gotoRoot();
-}
-
-void OptiTree::NaturalOpti(VectorXi & He)
-{
-    if(He.size()<=0)return;
-    He.array()-=He.minCoeff();
-    HeightLine HL(He);
-    BuildTree(HL);
-    gotoRoot();
-    Compress(HL);
-    He=HL.Height;
-}
-
-void OptiTree::BuildTree(HeightLine &HL)
-{
-    vector<short>index;string Brackets;
-    HL.toBrackets(index,Brackets);
-    Freeze();gotoRoot();Root->Value.setEmpty();
-    disp();
-    qDebug("括号式构建完成，开始构造优化树");
-    for(unsigned int i=0;i<index.size();i++)
-    {
-        if(Brackets[i]=='(')
-        {//qDebug()<<"rua!";
-            //遇到左括号，如果当前节点的Beg无效，则写入；如果有效且当前节点已完成，则创建侧树；否则创建子树
-            if(!Current()->Value.isEmpty())
-            {//遇到左括号，如果当前节点完整，则创建侧链
-                addSib(index[i]);
-                goNextSib();
-            }
-            else
-            {//遇到左括号且当前节点不完整
-                if(Current()->Value.getBegin()<0)
-                {
-                    Current()->Value.setBegin(index[i]);
-                }
-                else//遇到左括号且当前节点前完整后不完整，则创建子树
-                {
-                    addChild(index[i]);
-                    goNextSib();
-                }
-
-            }
-            continue;
-        }
-        if(Brackets[i]==')')
-        {//遇到右括号
-            //如果当前节点完整，则向上，写入end
-            //如果当前节点不完整，写入end
-            if(Current()->Value.isEmpty())
-            {
-                goUp();Current()->Value.setEnd(index[i]);
-            }
-            else
-            {
-                Current()->Value.setEnd(index[i]);
-            }
-        }
-    }
-    qDebug("BuildTree构建完成");
-    disp();
-}
-
-void OptiTree::Compress(HeightLine &HL)
-{
-    HL.Sink(Current()->Value);
-    if(Current()->haveChild())
-    {
-        goDown();
-        Compress(HL);
-    }
-    if(Current()->haveSib())
-    {
-        goNextSib();
-        Compress(HL);
-    }
-
-}
-
-void OptiTree::disp()
-{
-    cout<<endl;
-Root->disp();
-cout<<endl;
 }
--- optitree.h
+++ optitree.h
@@ -13,30 +13,7 @@
 //#include "heightline.cpp"
 using namespace std;
 using namespace Eigen;
-class idpRegion
-{
-public:
-    idpRegion(short beg=-1,short end=-1);
-    idpRegion(const idpRegion&);
-    void setBegin(short);
-    void setEnd(short);
-    short getBegin();
-    short getEnd();
-    bool isEmpty();
-    void setEmpty();
-    short Length();
-    idpRegion operator=(const idpRegion&);
-    bool operator==(const idpRegion&);
-    bool operator>(const idpRegion&);
-    bool operator<(const idpRegion&);
-    bool operator>=(const idpRegion&);
-    bool operator<=(const idpRegion&);
-    bool operator!=(const idpRegion&);
-private:
-    short Begin;
-    short End;
-};
-
+class Node;
 class HeightLine
 {
 public:
@@ -46,31 +23,28 @@ public:
     VectorXi Height;
     QImage toQImage();
     void toBrackets(vector<short>&index,string&brackets);
-    void Sink(idpRegion&);
+    void Sink(Node*);
 };
 
 class Node//二叉链，与二叉树没有本质区别
 {
 public:
+    short Begin;
+    short End;
     Node(short beg=-1,short end=-1);
-    Node(const idpRegion&);
     ~Node();
     void disp();
-private:
     Node *Child;
-    Node *Sib;//NextSib
-    idpRegion Value;
+    Node *Sib;
     bool isAble;
     short Degree;
-    friend class OptiTree;
-    void setEnable(bool able=false);
-    bool isLeaf();//只考虑Child，不考虑Sib
+    bool isComplete();
+    void Freeze();
     bool haveChild();//指有无激活的子树
     bool haveSib();//指有无激活的平级树
-    void creatChild();
-    void creatSib();
-    void insertChild(Node*);
-    void setValue(short =-1,short =-1);
+
+    Node* creatChild();
+    Node* creatSib();
 };
 
 class OptiTree
@@ -78,26 +52,15 @@ class OptiTree
 public:
     OptiTree();
     Node *Current();
-    void NaturalOpti(VectorXi&);
     ~OptiTree();//析构函数，删除所有节点
-    void disp();
-private:
     void goUp();
     void goDown();
     void goNextSib();
     void goPrevSib();
     bool isRoot();
-    bool isLeaf();
-    void addChild(short=-1,short=-1);
-    //void addChild(const idpRegion&);
-    void addSib(short=-1,short=-1);
-    //void addSib(const idpRegion&);
-
-    void Freeze();//冻结下面的所有子树
-    void Clear();//删除根节点外所有节点
 
     void gotoRoot();
-
+    void NaturalOpti(VectorXi&);
     void BuildTree(HeightLine&);
     void Compress(HeightLine&);
 
